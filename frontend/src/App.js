2b07d55306d9f4588ef55747b4e937a8e62826b7
Károly Török <torok.karoly.krisztian@gmail.com>
2020-10-08 19:54:46 +0200

    Bootsrap style added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
 
 export const App = () => {
   return (
     <>
-      <h1>Welcome to EShop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to EShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default App;
