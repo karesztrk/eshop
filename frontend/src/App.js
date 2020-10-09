f24829f47ec350fb938382c2efdcdfa1559b605d
Károly Török <torok.karoly.krisztian@gmail.com>
2020-10-09 15:08:25 +0200

    Product list view added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
 
 export const App = () => {
   return (
@@ -9,7 +10,7 @@ export const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to EShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
