558acc21a2676a556ec5e9b8500fc41cd87e7cfc
Károly Török <torok.karoly.krisztian@gmail.com>
2020-10-11 15:07:53 +0200

    Data schema added

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import colors from 'colors';
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -20,4 +23,6 @@ app.get('/api/products/:id', (request, response) => {
 const PORT = process.env.PORT || 5000;
 const ENV = process.env.NODE_ENV;
 
-app.listen(PORT, () => console.log(`Server started in ${ENV} on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server started in ${ENV} on port ${PORT}`.yellow.bold),
+);
