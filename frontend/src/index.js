e9293ed79fe5cb57a37e067ed48649eebc4f2812
Károly Török <torok.karoly.krisztian@gmail.com>
2020-10-12 21:07:45 +0200

    Redux plugin added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import './bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
