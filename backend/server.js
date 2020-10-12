05b4cfd5ec8cc71043645a97c2ea22bdfad937d5
Károly Török <torok.karoly.krisztian@gmail.com>
2020-10-12 20:55:11 +0200

    Error handler added

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import products from './data/products.js';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
+import productRoutes from './routes/productRoutes.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 connectDB();
@@ -12,13 +13,12 @@ const app = express();
 app.get('/', (request, response) => {
   response.send('API is running...');
 });
-app.get('/api/products', (request, response) => {
-  response.json(products);
-});
-app.get('/api/products/:id', (request, response) => {
-  const product = products.find((p) => p._id === request.params.id);
-  response.json(product);
-});
+
+//app.use(notFound);
+
+//app.use(errorHandler);
+
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 const ENV = process.env.NODE_ENV;
